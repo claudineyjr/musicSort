13dbb60738fc55fcc0f51da277a7d1ae75db236f
claudineyjr <claudiney.alves@fatec.sp.gov.br>
2017-09-30 15:09:18 -0300

    Consign route change

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-var app = require('express')();
-
-app.get('/', function(req, res) {
-    res.send('Tudo ok');
-});
-
+var app = require('./config/server');
 
 app.listen(5100, function() {
     console.log('Server working on port 5100');
