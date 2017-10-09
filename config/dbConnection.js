333cb4e520800d83e79beb45cb1964714aceabdc
Claudiney Junior <claudineyjunior@claudineys-air.bs.br.bsch>
2017-10-09 17:43:56 -0300

    Bluemix integration ok

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -8,25 +8,25 @@ var services = appenv.services;
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 3000);
 
-// for (var svcName in services) {
-//     if (svcName.match(/^mysql/)) {
-//       var mysqlCreds = services[svcName][0]['credentials'];
-//       var connMySql = function(){
-//         mysql.createConnection({
-//             host: mysqlCreds.host,
-//             port: mysqlCreds.port,
-//             user: mysqlCreds.user,
-//             password: mysqlCreds.password,
-//             database: mysqlCreds.name
-//         });   
-//       }
-//     }
-// }
+var connMySql = null;
 
-// if (process.env.VCAP_SERVICES) {
-//     var services = JSON.parse(process.env.VCAP_SERVICES);
-//     var mysql_creds = services['mysql'][0]['credentials'];
-// }
+if (process.env.VCAP_SERVICES) {
+    console.log('tentando conexao');
+    var services = JSON.parse(process.env.VCAP_SERVICES);
+    var mysqlCreds = services['cleardb'][0]['credentials'];
+    connMySql = function(){
+        return mysql.createConnection({
+            host: mysqlCreds.hostname,
+            port: mysqlCreds.port,
+            user: mysqlCreds.username,
+            password: mysqlCreds.password,
+            database: mysqlCreds.name
+        });   
+      }
+} else {
+    console.error('VCAP nao encontrado');
+}
+
 
 
 module.exports = function(){
